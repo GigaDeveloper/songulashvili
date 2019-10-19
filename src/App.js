5f1e7fd46d6ca20d5e4ebf56262228ddd6f66ba8
Giga Songulashvili <giga_songulashvili@epam.com>
2019-10-19 17:37:24 +0200

    Update home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Songulashvili</h1>
+      <ul>
+        <li>React app</li>
+        <li>Hosted by github pages</li>
+        <li>Deployed by gh-pages package</li>
+      </ul>
     </div>
   );
 }
